164617ee18d9b270c0b0572972c2830b9f32540e
Maximilian Erren <Maximilian.Erren@telekom.de>
2023-03-01 13:28:06 +0100

    give persistor close method

--- pkg/drp_player/measuresession/persistor.go
+++ pkg/drp_player/measuresession/persistor.go
@@ -18,6 +18,7 @@ type MeasureSessionPersistor struct {
 	session           *datatypes.DB_session
 	persist_frequency time.Duration
 	db                *persistence.Persistence
+	exit_persistor    chan uint8
 	// internal, anonymous, representation of needed io objects
 	csv *struct {
 		PacketFile   *os.File
@@ -35,6 +36,7 @@ func NewMeasureSessionPersistor(session *datatypes.DB_session) (*MeasureSessionP
 	mp := &MeasureSessionPersistor{
 		session:           session,
 		persist_frequency: 1 * time.Second,
+		exit_persistor:    make(chan uint8),
 	}
 	if session.ParentBenchmark.PrintToStdOut {
 		fmt.Println(strings.Join(assets.CONST_HEADING, " "))
@@ -93,7 +95,7 @@ func (s *MeasureSessionPersistor) init_csv() error {
 
 // Only has an effect if csv writing is active
 // Flushes all Wirters and Closes all opened files.
-func (s *MeasureSessionPersistor) Close() {
+func (s *MeasureSessionPersistor) close() {
 	DEBUG.Println("Closing MeasureSessionPersistor")
 	if s.csv != nil {
 		s.csv.QueueWriter.Flush()
@@ -101,81 +103,94 @@ func (s *MeasureSessionPersistor) Close() {
 		s.csv.QueueFile.Close()
 		s.csv.PacketFile.Close()
 	}
+	(*s.db).Commit()
+	close(s.exit_persistor)
 }
-func (s *MeasureSessionPersistor) persist(sample interface{}, r util.RoutineReport) {
+func (s *MeasureSessionPersistor) persist(sample interface{}) error {
 	if err := (*s.db).Persist(sample); err != nil {
-		r.Send_error_c <- fmt.Errorf("persisting sample (%+v) to DB: %w", sample, err)
+		//FATAL!
+		return fmt.Errorf("persisting sample (%+v) to DB: %w", sample, err)
 	}
 	if measure_packet, ok := sample.(DB_measure_packet); ok && s.session.ParentBenchmark.PrintToStdOut {
 		if err := measure_packet.PrintLine(); err != nil {
-			WARN.Println("Could not write Measurement")
-			r.Send_error_c <- err
-			//Todo check wg behaviour
-			//r.Wg.Done()
-			return
+			return fmt.Errorf("could not write Measurement: %w", err)
 		}
 	}
 	if s.csv == nil {
-		return
+		return nil
 	}
-	if csvsample, ok := sample.(datatypes.DB_measure_queue); ok {
+	if csvsample, ok := sample.(*datatypes.DB_measure_queue); ok {
 		//DEBUG.Printf("Persisitng DB_measure_queue: %v", csvsample)
 		if err := s.csv.QueueWriter.Write((csvsample).CsvRecord()); err != nil {
-			r.Send_error_c <- fmt.Errorf("persisting DB_measure_queue (%+v) to csv file: %w", sample, err)
+			return fmt.Errorf("persisting DB_measure_queue (%+v) to csv file: %w", sample, err)
 		}
-		return
+		return nil
 	}
 	if csvsample, ok := sample.(datatypes.DB_measure_packet); ok {
 		//DEBUG.Printf("Persisitng DB_measure_packet: %v", csvsample)
 		if err := s.csv.PacketWriter.Write((csvsample).CsvRecord()); err != nil {
-			r.Send_error_c <- fmt.Errorf("persisting DB_measure_packet (%+v) to csv file: %w", sample, err)
+			return fmt.Errorf("persisting DB_measure_packet (%+v) to csv file: %w", sample, err)
 		}
-		return
+		return nil
 	}
 	INFO.Printf("MeasureSessionPersistor.persist(%+v): unknown behavior", sample)
+	return nil
 }
 
-// Stars Loop, which persits any samples comin in throug the samples channel
+// Stars Loop, which persits any samples coming in through the samples channel
 //
 // Blocking, releases Wg
-func (s *MeasureSessionPersistor) Run(r util.RoutineReport, samples chan interface{}) {
+func (s *MeasureSessionPersistor) Run(samples chan interface{}, report_error func(err error, lvl util.ErrorLevel), done func()) {
 	//Setup
 	var err error
 	s.db, err = persistence.GetPersistence()
 	if err != nil {
-		r.Send_error_c <- fmt.Errorf("persistMeasures: %w", err)
+		report_error(fmt.Errorf("persistMeasures: %w", err), util.ErrWarn)
 	}
 	tickerPersist := time.NewTicker(s.persist_frequency)
 	for {
 		select {
-		case <-r.On_extern_exit_c:
-			DEBUG.Println("Closing persistor outer")
-			r.Wg.Done()
+		case <-s.exit_persistor:
+			s.close()
 			return
-		case <-tickerPersist.C: //start := time.Now()
-
+		case <-tickerPersist.C:
+			DEBUG.Println("Ticker")
 			readSamples := true
 			for readSamples {
 				select {
-				case sampleInterface := <-samples:
-					switch sampleInterface.(type) {
+				case sampleInterface, ok := <-samples:
+					if !ok {
+						DEBUG.Println("Closing persistor due to closed channel")
+						done()
+						return
+					}
+					switch sample := sampleInterface.(type) {
 					// write measure to db
-					case DB_measure_packet, DB_measure_queue:
-						s.persist(sampleInterface, r)
+					case DB_measure_packet:
+						err := s.persist(sample)
+						if err != nil {
+							report_error(err, util.ErrFatal)
+						}
+					case DB_measure_queue:
+						err := s.persist(&sample)
+						if err != nil {
+							report_error(err, util.ErrFatal)
+						}
 					default:
-						WARN.Printf("Unexpected Input in persistMeasures %+v", sampleInterface)
+						report_error(
+							fmt.Errorf("unexpected Input in persistMeasures %v", sampleInterface),
+							util.ErrWarn)
 					}
 				default:
 					readSamples = false
 				}
 			}
-			(*s.db).Commit()
-			//to csv
-			if s.csv != nil {
-				s.csv.PacketWriter.Flush()
-				s.csv.QueueWriter.Flush()
-			}
-
+		}
+		(*s.db).Commit()
+		//to csv
+		if s.csv != nil {
+			s.csv.PacketWriter.Flush()
+			s.csv.QueueWriter.Flush()
 		}
 	}
 }
